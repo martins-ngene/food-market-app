bf3aa666f1dab89f8decab1cead5f5d1abd17f7f
Martins Ngene <martinsngene9@gmail.com>
2024-01-01 23:28:26 +0100

    update: Build Splash Screen

---
+++ pages/home/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EB0029",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 191,
+    height: 178,
+  },
+});
