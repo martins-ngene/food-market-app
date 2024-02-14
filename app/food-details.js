76aad104655c7685606203e2dc5f4011537c6cfb
Martins Ngene <martinsngene9@gmail.com>
2024-02-14 22:49:23 +0100

    update: add order quantity mutation function and UI

--- app/food-details.js
+++ app/food-details.js
@@ -1,5 +1,5 @@
 import { Image, Pressable, StyleSheet, Text, View, Link } from "react-native";
-import React from "react";
+import { useState } from "react";
 import { Stack, useNavigation } from "expo-router";
 import { StarRatingDisplay } from "react-native-star-rating-widget";
 
@@ -9,8 +9,19 @@ import { Button } from "../components/buttons";
 const FoodDetails = () => {
   const navigation = useNavigation();
 
+  const [qtyValue, setQtyValue] = useState(0);
+
   //   Rating Value
   let rating = 3;
+
+  const changeQtyVal = action => {
+    if (action === "increment") {
+      setQtyValue(qtyValue + 1);
+    }
+    if (action === "decrement" && qtyValue !== 0) {
+      setQtyValue(qtyValue - 1);
+    }
+  };
   return (
     <View>
       <Stack.Screen
@@ -27,10 +38,27 @@ const FoodDetails = () => {
         <Image style={styles.image} source={icons.card} />
       </View>
       <View style={styles.details}>
-        <Text style={styles.heading}>Cherry Healthy</Text>
-        <View style={styles.rating}>
-          <StarRatingDisplay rating={rating} color='#EB0029' starSize={16} />
-          <Text style={styles.ratingValue}>{rating}</Text>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.heading}>Cherry Healthy</Text>
+            <View style={styles.rating}>
+              <StarRatingDisplay
+                rating={rating}
+                color='#EB0029'
+                starSize={16}
+              />
+              <Text style={styles.ratingValue}>{rating}</Text>
+            </View>
+          </View>
+          <View style={styles.itemQtyContainer}>
+            <Pressable onPress={() => changeQtyVal("decrement")}>
+              <Text style={styles.qtyBtn}>-</Text>
+            </Pressable>
+            <Text style={styles.qtyVal}>{qtyValue}</Text>
+            <Pressable onPress={() => changeQtyVal("increment")}>
+              <Text style={styles.qtyBtn}>+</Text>
+            </Pressable>
+          </View>
         </View>
         <Text style={styles.text}>
           Makanan khas Bandung yang cukup sering dipesan oleh anak muda dengan
@@ -110,6 +138,11 @@ const styles = StyleSheet.create({
     fontStyle: "normal",
     fontFamily: "Poppins_400Regular",
   },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
   heading: {
     fontSize: 16,
     fontStyle: "normal",
@@ -131,5 +164,28 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+  },
+  itemQtyContainer: {
+    width: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  qtyBtn: {
+    width: 26,
+    height: 26,
+    textAlign: "center",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontFamily: "Poppins_400Regular",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#020202",
+  },
+  qtyVal: {
+    fontSize: 16,
+    fontStyle: "normal",
+    fontFamily: "Poppins_400Regular",
+    textAlign: "center",
   },
 });
