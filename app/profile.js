b3adc7b3e9d783d6e0b676a68a27a9399de810af
Martins Ngene <martinsngene9@gmail.com>
2024-02-15 21:34:21 +0100

    update: build profile route

--- app/profile.js
+++ app/profile.js
@@ -1,19 +1,69 @@
 import { Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { icons } from "../icons";
 
 const Profile = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Stack.Screen
         options={{
           headerShown: false,
         }}
       />
-      <Text>Profile</Text>
+
+      <View style={styles.userProfile}>
+        <View style={styles.imageContainer}>
+          <Image source={icons.avatar} style={styles.image} />
+        </View>
+        <Text style={styles.name}>Aashifa Sheikh</Text>
+        <Text style={styles.email}>contact.uiuxexperts@gmail.com</Text>
+      </View>
     </View>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 50,
+  },
+  userProfile: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "100%",
+    height: 230,
+    fontFamily: "Poppins_400Regular",
+    paddingVertical: 24,
+  },
+  imageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 110,
+    height: 110,
+    padding: 24,
+    borderRadius: 90,
+    borderColor: "#8D92A3",
+    borderWidth: 1,
+    borderStyle: "dashed",
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 90,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "600",
+    fontFamily: "Poppins_400Regular",
+  },
+  email: {
+    fontSize: 14,
+    fontFamily: "Poppins_400Regular",
+    color: "#8D92A3",
+  },
+});
--- app/signup.js
+++ app/signup.js
@@ -97,7 +97,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    // justifyContent: "center",
     paddingTop: 50,
     paddingHorizontal: 24,
   },
